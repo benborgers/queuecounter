183a8fe2ca9a9927fd31e33abaa2427d02b757a5
Ben Borgers <borgersbenjamin@gmail.com>
2025-02-25 19:32:50 -0500

    Back to every 2 seconds

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use App\Console\Commands\CheckQueue;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(CheckQueue::class)
-    ->everySecond()
+    ->everyTwoSeconds()
     ->between('9:00', '23:59')
     ->timezone('America/New_York')
     ->runInBackground();
